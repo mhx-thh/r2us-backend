cb2e022abf4c591f7ce3f8a11a3a0f0cee586df8
anthony2708 <builetuananh2708@gmail.com>
2021-07-26 10:10:02 +0700

    210726 - BLTAnh - Fix structure

--- routes/classRouter/review.js
+++ routes/classRouter/review.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const reviewController = require('../controller/reviewCtrl');
-const authController = require('../controller/authCtrl');
+const reviewController = require('../../controller/reviewCtrl');
+const authController = require('../../controller/authCtrl');
 
 const router = express.Router({ mergeParams: true });
 
