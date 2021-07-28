a255c67c9adb0cb04bea01027d90c1abcfdee975
anthony2708 <builetuananh2708@gmail.com>
2021-07-28 10:52:40 +0700

    210728 - BLTAnh - API Academic Finished - Need testing

--- routes/academicRouter/course.js
+++ routes/academicRouter/course.js
@@ -2,12 +2,13 @@ const express = require('express');
 const authController = require('../../controller/authCtrl');
 const courseController = require('../../controller/courseCtrl');
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 
 router
   .route('/')
   .get(courseController.getAllCourses)
   .post(
+    authController.protect,
     authController.restrictTo('admin'),
     courseController.createCourse,
   );
@@ -16,10 +17,12 @@ router
   .route('/:id')
   .get(courseController.getCourse)
   .patch(
+    authController.protect,
     authController.restrictTo('admin'),
     courseController.updateCourse,
   )
   .delete(
+    authController.protect,
     authController.restrictTo('admin'),
     courseController.deleteCourse,
   );
--- routes/academicRouter/faculty.js
+++ routes/academicRouter/faculty.js
@@ -2,14 +2,13 @@ const express = require('express');
 const authController = require('../../controller/authCtrl');
 const facultyController = require('../../controller/facultyCtrl');
 
-const router = express.Router({ mergeParams: true });
-
-router.use(authController.protect);
+const router = express.Router();
 
 router
   .route('/')
   .get(facultyController.getAllFaculty)
   .post(
+    authController.protect,
     authController.restrictTo('admin'),
     facultyController.createFaculty,
   );
@@ -18,10 +17,12 @@ router
   .route('/:id')
   .get(facultyController.getFaculty)
   .patch(
+    authController.protect,
     authController.restrictTo('admin'),
     facultyController.updateFaculty,
   )
   .delete(
+    authController.protect,
     authController.restrictTo('admin'),
     facultyController.deleteFaculty,
   );
